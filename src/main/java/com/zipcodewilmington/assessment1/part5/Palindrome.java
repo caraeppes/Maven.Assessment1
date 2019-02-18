4c3dbf94940b1f5419454fa39d6193d2584e8787
Cara Eppes <caraeppes@zipcoders-MacBook-Pro-4.local>
2019-02-18 12:24:19 -0500

    completed part 5.  all 70 tests pass

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,31 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import static com.zipcodewilmington.assessment1.part1.BasicStringUtils.reverse;
+
 public class Palindrome {
 
+    public Boolean isPalindrome(String s){
+        if (s.equals(reverse(s))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public Integer countPalindromes(String input){
-        return null;
+        int substringLength = 2;
+        Integer answer = input.length();
+        while(substringLength <= input.length()){
+            for (int i = 0; i < input.length(); i++){
+                try {
+                    if (isPalindrome(input.substring(i, i + substringLength))) {
+                        answer++;
+                    }
+                } catch (StringIndexOutOfBoundsException e){}
+            }
+            substringLength++;
+        }
+        return answer;
     }
 }
