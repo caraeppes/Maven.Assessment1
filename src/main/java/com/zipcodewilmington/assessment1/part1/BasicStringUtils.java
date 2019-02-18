4b767c3b4f803a74dd336bf57f3ca8f505f982e2
Cara Eppes <caraeppes@zipcoders-MacBook-Pro-4.local>
2019-02-18 09:36:21 -0500

    completed BasicArrayUtils and BasicStringUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,7 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        return Character.toString(str.charAt(0)).toUpperCase() + str.substring(1);
     }
 
     /**
@@ -17,7 +17,12 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String answer = "";
+        String[] arr = str.split("");
+        for (int i = 0; i < arr.length; i++) {
+            answer = arr[i] + answer;
+        }
+        return answer;
     }
 
     /**
@@ -25,7 +30,7 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        return camelCase((reverse(str)));
     }
 
 
@@ -34,7 +39,7 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1, str.length() - 1);
     }
 
     /**
@@ -42,6 +47,15 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLowerCase(str.charAt(i))) {
+                answer = answer + Character.toString(str.charAt(i)).toUpperCase();
+            } else {
+                answer = answer + Character.toString(str.charAt(i)).toLowerCase();
+            }
+        }
+        return answer;
     }
 }
+
